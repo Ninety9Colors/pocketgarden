c480b3c1d7c4ecf4b9efc0325407b19124eb956f
Darek <darekzhang0629@gmail.com>
2025-10-21 16:34:52 -0500

    Made framerate update once per second. Optimizing lighting data with shaders, still need to clean up event and keybind system

--- src/application.cpp
+++ src/application.cpp
@@ -22,6 +22,7 @@ constexpr int DEFAULT_SCREEN_HEIGHT = 720;
 constexpr int FONT_SIZE = 40;
 
 Application::Application() : ip_({0}), port_({0}), username_({0}), ip_focus_(false), port_focus_(false), username_focus_(false) {
+    DEBUG("Initializing window with size " + std::to_string(DEFAULT_SCREEN_WIDTH) + "," + std::to_string(DEFAULT_SCREEN_HEIGHT));
     InitWindow(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT, "PocketGarden");
     SetWindowSize(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT);
     SetExitKey(KEY_NULL);
@@ -55,42 +56,63 @@ void Application::run(Game& game) {
     DEBUG("Starting application...");
     MainCamera main_camera {};
 
-    float tps = 20.0f;
-    float dt_tick = 0;
-    int weather_update_interval = 300; // seconds
-    int64_t last_weather_update = 0;
-    uint32_t total_ticks = 0;
+    const float TPS = 20.0f; // ticks per second
+    float dt_tick = 0; // time elapsed since last tick (seconds)
+    uint64_t total_ticks = 0;
 
-    int sunPosLoc = GetShaderLocation(*shader_default_,"sunPos");
-    int sunColorLoc = GetShaderLocation(*shader_default_,"sunColor");
-    int ambientLoc = GetShaderLocation(*shader_default_,"ambient");
+    const int WEATHER_UPDATE_INTERVAL = 300; // (seconds)
+    uint64_t last_weather_update = 0;
 
-    Spline spline_test {};
+    const int UI_UPDATE_INTERVAL = 1; // (seconds)
+    uint64_t last_ui_update = 0;
+
+    int sun_position_loc = GetShaderLocation(*shader_default_,"sunPos");
+    int sun_color_loc = GetShaderLocation(*shader_default_,"sunColor");
+    int ambient_loc = GetShaderLocation(*shader_default_,"ambient");
+
+    std::string fps_buffer;
 
     while (!WindowShouldClose()) {
-        int64_t current_timestamp = std::time(nullptr);
         if (!game.in_world()) {
             display_menu(game);
             continue;
         }
-        float dt = GetFrameTime(); // seconds
+
+        uint64_t current_timestamp = std::time(nullptr);
+        float dt = GetFrameTime(); // elapsed seconds of last frame (seconds)
         dt_tick += dt;
-        std::string fps = std::to_string((int)round(1.0/dt));
+
         std::vector<bool> keybinds = {IsKeyDown(KEY_W), IsKeyDown(KEY_A), IsKeyDown(KEY_S), IsKeyDown(KEY_D), IsKeyDown(KEY_TAB), IsKeyDown(KEY_ESCAPE),
                                         IsMouseButtonPressed(MOUSE_LEFT_BUTTON), (GetMouseWheelMoveV().y > 0), (GetMouseWheelMoveV().y < 0), IsKeyPressed(KEY_SPACE), IsKeyDown(KEY_Q), IsKeyDown(KEY_E), IsKeyPressed(KEY_R)};
         game.poll_events(game.get_current_user(), game.get_world(), game.get_network(), game, current_timestamp, event_buffer_, main_camera, keybinds,dt,shader_default_);
+        
         const auto player = game.get_current_player();
-        if (player == nullptr)
-            continue;
+        if (player == nullptr) {
+            CRITICAL("Could not find player!");
+            break;
+        }
+
         player->update(event_buffer_, main_camera, game.get_world(), keybinds, dt);
-        if (dt_tick >= 1/tps) {
-            if (current_timestamp-last_weather_update >= weather_update_interval && game.get_network()->is_host()) {
+        if (dt_tick >= 1.0f/TPS) {
+            if (current_timestamp-last_weather_update >= WEATHER_UPDATE_INTERVAL && game.get_network()->is_host()) {
                 bool updated = game.get_world()->get_weather()->update();
                 if (updated) {
+                    DEBUG("Updating weather information in world and shader...");
                     event_buffer_["WeatherUpdateEvent"] = std::make_shared<WeatherUpdateEvent>(game.get_world()->get_weather()->get_weather_id());
                     last_weather_update = current_timestamp;
                     game.get_world()->get_weather()->update_sun(current_timestamp);
                     game.get_world()->update_sun();
+
+                    // Pass new lighting information to shader
+                    const Vector3 sun_position = game.get_world()->get_sun()->get_position();
+                    float sun_pos[3] = {sun_position.x, sun_position.y, sun_position.z};
+                    SetShaderValue(*shader_default_, sun_position_loc, sun_pos, SHADER_UNIFORM_VEC3);
+                    float sun_color[4] = {1.0f,1.0f,(std::pow(std::max(sun_position.y,0.0f),2)/10000.0f),1.0f};
+                    SetShaderValue(*shader_default_, sun_color_loc, sun_color, SHADER_UNIFORM_VEC4);
+
+                    float ambient_level = (std::pow(std::max(sun_position.y,0.0f),2)/10000.0f)*0.5f + 0.25f;
+                    float ambient[4] = {ambient_level,ambient_level,ambient_level,1.0f};
+                    SetShaderValue(*shader_default_, ambient_loc, ambient, SHADER_UNIFORM_VEC4);
                 }
             }
             tick(event_buffer_, game);
@@ -99,18 +121,8 @@ void Application::run(Game& game) {
         }
         main_camera.update(player, GetMouseDelta());
 
-        float cameraPos[3] = {main_camera.get_position().x, main_camera.get_position().y, main_camera.get_position().z};
-        SetShaderValue(*shader_default_, shader_default_->locs[SHADER_LOC_VECTOR_VIEW], cameraPos, SHADER_UNIFORM_VEC3);
-
-        Vector3 sun_position = game.get_world()->get_sun()->get_position();
-        float sunPos[3] = {sun_position.x, sun_position.y, sun_position.z};
-        SetShaderValue(*shader_default_, sunPosLoc, sunPos, SHADER_UNIFORM_VEC3);
-        float sunColor[4] = {1.0f,1.0f,(std::pow(std::max(sun_position.y,0.0f),2)/10000.0f),1.0f};
-        SetShaderValue(*shader_default_, sunColorLoc, sunColor, SHADER_UNIFORM_VEC4);
-
-        float ambient_level = (std::pow(std::max(sun_position.y,0.0f),2)/10000.0f)*0.5f + 0.25f;
-        float ambient[4] = {ambient_level,ambient_level,ambient_level,1.0f};
-        SetShaderValue(*shader_default_, ambientLoc, ambient, SHADER_UNIFORM_VEC4);
+        float cam_pos[3] = {main_camera.get_position().x, main_camera.get_position().y, main_camera.get_position().z};
+        SetShaderValue(*shader_default_, shader_default_->locs[SHADER_LOC_VECTOR_VIEW], cam_pos, SHADER_UNIFORM_VEC3);
 
         BeginDrawing();
 
@@ -119,21 +131,24 @@ void Application::run(Game& game) {
         game.get_world()->get_sun()->draw();
         draw_players(game.get_current_user(), game.get_world()->get_players(), main_camera);
         draw_objects(game.get_world()->get_objects());
-        for (float t = 0; t <= spline_test.size()-1; t+=0.1f)
-            DrawSphere(spline_test.get(t),0.1f,ORANGE);
         EndMode3D();
+
         // Crosshair
         DrawCircle(GetScreenWidth()/2,GetScreenHeight()/2,3,WHITE);
 
-        int fps_size = MeasureText(fps.c_str(),FONT_SIZE);
-        GuiLabel((Rectangle){0,0,fps_size,FONT_SIZE},fps.c_str());
+        // Debug UI
+        if (current_timestamp-last_ui_update >= UI_UPDATE_INTERVAL) {
+            INFO("Updating FPS");
+            fps_buffer = std::to_string((int)round(1.0f/dt));
+            last_ui_update = current_timestamp;
+        }
+        int fps_size = MeasureText(fps_buffer.c_str(),FONT_SIZE);
+        GuiLabel((Rectangle){0,0,fps_size,FONT_SIZE},fps_buffer.c_str());
+
         if (keybinds[4]) {display_scoreboard(game.get_world()->get_players());}
         EndDrawing();
         if (keybinds[5]) {game.disconnect();}
-        if (IsKeyPressed(KEY_G))
-            spline_test.add(player->get_position());
     }
-    DEBUG("Closing application...");
     game.disconnect();
 }
 
@@ -215,6 +230,7 @@ void Application::draw_players(std::string current_user, const std::vector<std::
 }
 
 void Application::exit() {
+    DEBUG("Closing Window");
     CloseWindow();
 }
 
